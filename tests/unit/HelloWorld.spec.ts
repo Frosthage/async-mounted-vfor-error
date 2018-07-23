f920c40e2f30fa215ad64ea6f393daac15cfd91f
Mikael Frosthage <mikael.frosthage@arcam.com>
2018-07-23 10:42:40 +0200

    ...

--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -3,7 +3,7 @@ import BeforeSleep from '@/components/BeforeSleep.vue';
 import AfterSleep from '@/components/AfterSleep.vue';
 
 describe('BeforeSleep.vue', () => {
-  it('renders props.msg when passed', async () => {
+  it('Assigns array before sleep', async () => {
 
     const localVue = createLocalVue();
     localVue.config.productionTip = true;
@@ -18,18 +18,15 @@ describe('BeforeSleep.vue', () => {
 });
 
 describe('AfterSleep.vue', () => {
-  it('renders props.msg when passed', async () => {
+  it('Assigns array after sleep', async () => {
 
     const localVue = createLocalVue();
     localVue.config.productionTip = true;
     const wrapper = shallowMount(AfterSleep, {
-
+      localVue,
     });
 
     await localVue.nextTick();
-
-
-
 
     expect(wrapper.findAll('.after h1').length).toBe(3);
   });
